212fc9893afeada5cf25bb32b254aec845d43215
Саша Воробьёв <avorobiev@deloitte.ru>
2020-08-26 19:25:32 +0300

    Вернул вывод ASCII таблицы

--- 004-ascii/main.go
+++ 004-ascii/main.go
@@ -1,14 +1,17 @@
 package main
 
-import "ASCIITableTools/randompassword"
+import (
+	"ASCIITableTools/randompassword"
+	"fmt"
+)
 
 func main() {
-	/*
-		fmt.Println("binary\tdecimal\thex\tcharacter")
 
-		for i := 32; i < 127; i++ {
-			fmt.Printf("%b\t%d\t%#x\t%c\n", i, i, i, i)
-		}
-	*/
+	fmt.Println("binary\tdecimal\thex\tcharacter")
+
+	for i := 32; i < 127; i++ {
+		fmt.Printf("%b\t%d\t%#x\t%c\n", i, i, i, i)
+	}
+
 	randompassword.NewRandomPassword()
 }
